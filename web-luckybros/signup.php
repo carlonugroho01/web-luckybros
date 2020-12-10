27a043daced1dbcbb7197f0948a9693dfb328d5a
carlonugroho01 <63065991+carlonugroho01@users.noreply.github.com>
2020-12-11 01:56:32 +0700

    Add files via upload

---
+++ web-luckybros/signup.php
@@ -0,0 +1,12 @@
+<?php
+include_once 'header.php';
+?>
+
+<section class="signup-form" id="services">
+    <h2>Sign Up</h2>
+    <form action="signup.inc.php" method="post">
+</section>
+
+<?php
+include_once 'footer.php';
+?>
